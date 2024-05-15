1303192f762f13bcb4e0af4d4b3e188e01bfd914
KubaK1P <1p22kus@gmail.com>
2024-05-15 10:28:36 +0200

    trzeba jescze naprawic o mnie

--- js/navbar.js
+++ js/navbar.js
@@ -4,7 +4,7 @@ function showList() {
   console.log("Call");
   if (navPlace.className == "menu-add-span") {
     navPlace.className += " activated";
-    navPlace.innerHTML = `<ul class="list"><li><a href="./html/choroby.html">Choroby</a></li><li><a href="#">O mnie</a></li><li><a href="#">O projekcie?</a></li></ul>`;
+    navPlace.innerHTML = `<ul class="list"><li><a href="./html/choroby.html">Choroby</a></li><li><a href="./html/omnie.html">O mnie</a></li><li><a href="./html/oprojekcie.html">O projekcie?</a></li></ul>`;
   } else {
     navPlace.className = "menu-add-span";
     navPlace.innerHTML = "";
